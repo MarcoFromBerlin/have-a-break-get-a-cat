35ebd181ff9281e117b68ef1dfd665a82b386d31
Marco Vignotto <marcovignotto81@gmail.com>
2021-01-21 10:30:50 +0100

    Promise ok  loading ok

--- dist/scripts/index.js
+++ dist/scripts/index.js
@@ -1,46 +1,52 @@
 // API
-// Create a request variable and assign a new XMLHttpRequest object to it.
-var request = new XMLHttpRequest();
 let isLoaded = false;
-// Open a new connection, using the GET request on the URL endpoint
-request.open(
-  "GET",
-  "https://api.thecatapi.com/v1/images/?limit=5&page=0&order=DESC&",
-  true
-);
 
-// throws back error: CORS header ‘Access-Control-Allow-Origin’ is ‘*’
-// request.withCredentials = true;
+function getUserDataWithPromise() {
+  var xhr = new XMLHttpRequest();
 
-request.setRequestHeader("Content-Type", "application/json");
+  return new Promise(function (resolve, reject) {
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState == 4) {
+        if (xhr.status >= 300) {
+          reject("Error, status code = " + xhr.status);
+        } else {
+          resolve(xhr.responseText);
+        }
+      }
+    };
+    xhr.open(
+      "GET",
+      "https://api.thecatapi.com/v1/images/?limit=5&page=0&order=DESC&",
+      true
+    );
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.setRequestHeader("x-api-key", "17d94b92-754f-46eb-99a0-65be65b5d18f");
+    xhr.send();
+  });
+}
 
-request.setRequestHeader("x-api-key", "17d94b92-754f-46eb-99a0-65be65b5d18f");
-// request.setRequestHeader("x-api-key", "e856f8ad-7b46-4dcb-8412-813d667e3204");
+getUserDataWithPromise()
+  .then(
+    function (result) {
+      var data = JSON.parse(result);
 
-request.onload = function () {
-  // Begin accessing JSON data here
-  var data = JSON.parse(this.response);
-  if (request.status >= 200 && request.status < 400) {
-    data.forEach((image) => {
-      const img = document.createElement("img");
-      img.className = "cat__img";
+      data.forEach((image) => {
+        const img = document.createElement("img");
+        img.className = "cat__img";
 
-      img.src = image.url;
-      document.querySelector(".cat__images__container").appendChild(img);
-    });
-    isLoaded = true;
-  } else {
-    console.log("error");
-  }
-};
-
-// Send request
-request.send();
+        img.src = image.url;
+        document.querySelector(".cat__images__container").appendChild(img);
+      });
+    },
+    function (error) {
+      console.log(error);
+    }
+  )
+  .then((isLoaded = true));
 
 // API END
 
 // OBSERVER
-
 if (isLoaded) {
   const curImage = document.querySelectorAll("cat__img");
   console.log(curImage);
