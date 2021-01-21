73ca31050a208f4fe11ddb5acdab2f31cd16f6b8
Marco Vignotto <marcovignotto81@gmail.com>
2021-01-21 17:52:11 +0100

    time limit added

--- dist/scripts/index.js
+++ dist/scripts/index.js
@@ -1,9 +1,10 @@
+let getTime = localStorage.getItem("timeObj");
+
 // API
 let isLoaded = false;
 
 function getUserDataWithPromise() {
   var xhr = new XMLHttpRequest();
-
   return new Promise(function (resolve, reject) {
     xhr.onreadystatechange = function () {
       if (xhr.readyState == 4) {
@@ -24,60 +25,94 @@ function getUserDataWithPromise() {
     xhr.send();
   });
 }
+// console.log(new Date() > new Date(JSON.parse(getTime)?.expiry));
+// console.log(!getTime);
+// if (!getTime) {
+if (!getTime || new Date() > new Date(JSON.parse(getTime)?.expiry)) {
+  getUserDataWithPromise().then(
+    function (result) {
+      var data = JSON.parse(result);
 
-getUserDataWithPromise().then(
-  function (result) {
-    var data = JSON.parse(result);
+      data.forEach((image) => {
+        const catImgCont = document.querySelector(".cat__images__container");
 
-    data.forEach((image) => {
-      const catImgCont = document.querySelector(".cat__images__container");
+        // set the width of the div container
+        catImgCont.style.width = `${
+          document.body.querySelector(".cat__images__main").offsetWidth
+        }px`;
 
-      // set the width of the div container
-      catImgCont.style.width = `${
+        const img = document.createElement("div");
+        img.className = "cat__div";
+
+        img.style.backgroundImage = `url('${image.url}')`;
+        img.style.width = `${
+          document.body.querySelector(".cat__images__main").offsetWidth
+        }px`;
+        img.style.height = `${
+          document.body.querySelector(".cat__images__main").offsetHeight
+        }px`;
+        img.style.transition = "all 0.5s";
+
+        document
+          .querySelector(".cat__images__container")
+          .insertBefore(img, document.querySelectorAll(".cat__div")[0]);
+        // document.querySelector(".cat__images__container").appendChild(img);
+      });
+      const endSlide = document.querySelector(".slide__end");
+      // endSlide.style.backgroundColor = "red";
+      endSlide.style.width = `${
         document.body.querySelector(".cat__images__main").offsetWidth
       }px`;
-
-      const img = document.createElement("div");
-      img.className = "cat__div";
-
-      img.style.backgroundImage = `url('${image.url}')`;
-      img.style.width = `${
-        document.body.querySelector(".cat__images__main").offsetWidth
-      }px`;
-      img.style.height = `${
+      endSlide.style.height = `${
         document.body.querySelector(".cat__images__main").offsetHeight
       }px`;
-      img.style.transition = "all 0.5s";
+      endSlide.style.transition = "all 0.5s";
 
-      document
-        .querySelector(".cat__images__container")
-        .insertBefore(img, document.querySelectorAll(".cat__div")[0]);
-      // document.querySelector(".cat__images__container").appendChild(img);
-    });
-    const endSlide = document.querySelector(".slide__end");
-    endSlide.style.backgroundColor = "red";
-    endSlide.style.width = `${
-      document.body.querySelector(".cat__images__main").offsetWidth
-    }px`;
-    endSlide.style.height = `${
-      document.body.querySelector(".cat__images__main").offsetHeight
-    }px`;
-    endSlide.style.transition = "all 0.5s";
-
-    // endSlide.innerHTML = "<h1>Paragraph changed!</h1>";
-    document.querySelector(".cat__images__container").appendChild(endSlide);
-  },
-  function (error) {
-    console.log(error);
-  }
-);
+      // endSlide.innerHTML = "<h1>Paragraph changed!</h1>";
+      document.querySelector(".cat__images__container").appendChild(endSlide);
+    },
+    function (error) {
+      console.log(error);
+    }
+  );
+}
+// }
 
 // API END
 
+// CHECK USER
+
+console.log(new Date()); // check if or not
+console.log(new Date(JSON.parse(getTime)?.expiry)); // check if or not
+
+console.log(new Date() > new Date(JSON.parse(getTime)?.expiry));
+
+function countEnd() {
+  if (count === totalSlides) {
+    // console.log("ennd");
+    let now = new Date();
+    // console.log(now);
+
+    const item = {
+      // value: value,
+      expiry: now.setSeconds(now.getSeconds() + 3),
+      // expiry: now.setHours(now.getHours() + 3),
+    };
+
+    localStorage.setItem("timeObj", JSON.stringify(item));
+    // if (!getTime)
+
+    // console.log(JSON.parse(getTime).expiry); // check if or not
+  }
+}
+// CHECK END
+
 // OBSERVER
+let count = 0;
 
 // MUST BE A NODE NOT A NODE LIST!!!
-var target = document.querySelector(".cat__images__container");
+var target1 = document.querySelector(".cat__images__container");
+// var target2 = count;
 var totalSlides;
 // create an observer instance
 var observer = new MutationObserver(function (mutations) {
@@ -85,7 +120,7 @@ var observer = new MutationObserver(function (mutations) {
   if (mutations.length > 0) {
     isLoaded = true;
     // console.log(document.querySelectorAll(".cat__div"));
-    console.log(totalSlides);
+    // console.log(totalSlides);
     totalSlides = document.querySelectorAll(".cat__div").length - 1;
   }
 });
@@ -94,16 +129,17 @@ var observer = new MutationObserver(function (mutations) {
 var config = { attributes: true, childList: true, characterData: true };
 
 // pass in the target node, as well as the observer options
-observer.observe(target, config);
+observer.observe(target1, config);
+// observer.observe(target2, config);
 
 // IMAGES
-let count = 0;
 
 const nextImg = (e) => {
-  // console.log(totalSlides);
-  // console.log(count);
-  if (e.target.classList.contains("cat__div")) {
-    console.log("click");
+  // console.log("vtotalSlides", totalSlides);
+  // console.log("count", count);
+  // console.log("count === totalSlides", count === totalSlides);
+  if (e.target.classList.contains("cat__div") && count !== totalSlides) {
+    // console.log("click");
     // TOTAL width - Div Width
     // console.log(e.target.offsetWidth);
     e.target.style.width = `${0}px`;
@@ -118,11 +154,10 @@ const nextImg = (e) => {
     //   e.target.offsetWidth
     // }px`;
     count++;
+
+    // triggers at counter end to set time
+    countEnd();
   }
-  // if (e.target.classList.contains("cat__div") && count >= totalSlides) {
-  //   console.log("end");
-  //   // e.target.style.width = `${1000}px`;
-  // }
 };
 
 document.body.addEventListener("click", nextImg);
